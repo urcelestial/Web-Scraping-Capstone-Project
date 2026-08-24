504ee8970b6071c030ac95dd2876cb4fa4a0915c
Rochelle <rochellecelesta17@gmail.com>
2026-08-24 00:06:33 -0400

    first draft

---
+++ WEB SCRAPING/cities.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+import json
+import pandas as pd
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+response = requests.get("https://www.timeanddate.com/weather/", headers=headers)
+
+driver.get("https://www.timeanddate.com/weather/")
+
+
+
+
+
+
+
+
+
